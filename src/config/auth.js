ada31a1a70147fd3ac356e5b63a95d20e62d8bb4
AkatGabrielGoncalves <gabrielmatheus9a@gmail.com>
2020-06-08 17:35:53 -0300

    Finish user register, start auth middleware

---
+++ src/config/auth.js
@@ -0,0 +1,5 @@
+export default {
+  secret: '87b76e492caf434e0fcf2cae1349855b',
+
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,14 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
+
+routes.put('/users', UserController.update);
 
 export default routes;
