1d8ef87420226f940e51b7cfd64c6bc075c8198f
AkatGabrielGoncalves <gabrielmatheus9a@gmail.com>
2020-06-19 18:35:17 -0300

    Add nodemailer, handlebars and email send funct

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -14,10 +14,22 @@ class NotificationController {
     const notifications = await Notification.find({
       user: provider.id,
     })
-      .sort('createdAt')
+      .sort({ createdAt: 'desc' })
       .limit(20);
 
     return res.json(notifications);
+  }
+
+  async update(req, res) {
+    // const Notification = await Notification.findById(req.params.id);
+
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
   }
 }
 
